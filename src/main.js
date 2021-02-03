07b2cfae3f7f159a885c7523e7dc573305c7ffe3
冯文祺 <fwq605732747@live.cn>
2021-02-03 17:47:53 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,12 +1,47 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-import router from './router'
+import routes from './router'
 import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let router = null;
+let instance = null;
+if (window.__POWERED_BY_QIANKUN__) { 
+  //处理资源
+   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__; 
+  }
+//下面的 /admin  与主应用  registerMicroApps 中的  activeRule 字段对应
+function render(props = {}) {
+  const {container} = props
+  router = new VueRouter({
+    base: window.__POWERED_BY_QIANKUN__ ? '/admin' : '/',
+    mode: 'history',
+    routes,
+  });
+  const create = async () => {
+    // await ReadConfig(Vue)
+    instance = new Vue({
+      router,
+      render: h => h(App),
+    }).$mount(container ? container.querySelector('#childApp') : '#childApp');
+  }
+  create()
+}
+if (!window.__POWERED_BY_QIANKUN__) {//判断是否在qiankun环境下
+  render();
+}
+// 导出子应用生命周期 挂载前
+export async function bootstrap(props) {
+  console.log(props)
+  console.log('[vue] vue app bootstraped');
+}
+export async function mount(props) {// 导出子应用生命周期 挂载前 挂载后
+  render(props);
+}// 导出子应用生命周期 挂载前 卸载后
+export async function unmount() {
+  instance.$destroy();
+  instance = null;
+  router = null;
+}
---
+++ vue.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const packageName = require('./package').name;
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+const dev = process.env.NODE_ENV === 'development'
+const port = 9528; // dev port
+module.exports = {
+  publicPath: dev ? `//localhost:${port}` : '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  filenameHashing: true,
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+    port,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  // 自定义webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+    output: {// 把子应用打包成 umd 库格式
+      library: `${packageName}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${packageName}`,
+    },
+    
+  },
+};
